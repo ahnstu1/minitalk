dc11a96b8dd9dfd34e4e17e290c3dd4a7b257dfa
Hyunggeun Ahn <hahn@c1r9s7.42seoul.kr>
2022-07-04 15:44:52 +0900

    wrong calc fix

--- client.c
+++ client.c
@@ -6,12 +6,11 @@
 /*   By: hahn <hahn@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/27 16:34:49 by hahn              #+#    #+#             */
-/*   Updated: 2022/06/27 17:11:18 by hahn             ###   ########.fr       */
+/*   Updated: 2022/07/04 15:38:50 by hahn             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
-#include <stdio.h>
 
 int	main(int argc, char **argv)
 {
@@ -25,18 +24,13 @@ int	main(int argc, char **argv)
 	idx = 0;
 	while (argv[2][idx])
 	{
-		printf("\n argv : %c \n", argv[2][idx]);
-		printf("\n test : %d \n", 1 << 7);
 		div = 128;
-		while (1)
+		while (div)
 		{
-			printf("\nif : %d \n", argv[2][idx] & div);
 			if (argv[2][idx] & div)
 				kill(pid, SIGUSR1);
 			else
 				kill(pid, SIGUSR2);
-			if (!div)
-				break;
 			div /= 2;
 			usleep(100);
 		}
--- server.c
+++ server.c
@@ -6,25 +6,18 @@
 /*   By: hahn <hahn@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/27 16:34:54 by hahn              #+#    #+#             */
-/*   Updated: 2022/06/27 16:34:55 by hahn             ###   ########.fr       */
+/*   Updated: 2022/07/04 15:40:40 by hahn             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-
 #include "minitalk.h"
-#include <stdio.h>
 
 void	handler(int signum)
 {
 	static int	count = 7;
 	static int	num = 0;
 
-//	num += signum == SIGUSR1;
-	if (signum == SIGUSR1)
-		num |= 1;
-	else if (signum == SIGUSR2)
-		num |= 0;
-	printf("\n num : %d \n", num);
+	num += signum == SIGUSR1;
 	count--;
 	if (count == -1)
 	{
@@ -38,11 +31,7 @@ void	handler(int signum)
 
 int main(void)
 {
-    char	*pid;
-
-	pid = ft_itoa(getpid());
-
-	write(1, pid, ft_strlen(pid));
+	ft_putendl_fd(ft_itoa(getpid()), 1);
     signal(SIGUSR1, handler);
 	signal(SIGUSR2, handler);
 	while (1)
