5191751785ed5742fb5f802be8e16401d1d23b3c
Hyunggeun Ahn <hahn@c1r9s7.42seoul.kr>
2022-07-06 13:51:49 +0900

    restore

--- client.c
+++ client.c
@@ -6,45 +6,35 @@
 /*   By: hahn <hahn@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/27 16:34:49 by hahn              #+#    #+#             */
-/*   Updated: 2022/07/06 10:40:07 by hahn             ###   ########.fr       */
+/*   Updated: 2022/07/06 13:51:00 by hahn             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
-void	send(char c, pid_t pid)
-{
-	int	div;
-
-	div = 128;
-	while (div)
-	{
-		if (c & div)
-			kill(pid, SIGUSR1);
-		else
-			kill(pid, SIGUSR2);
-		div /= 2;
-		usleep(100);
-	}
-}
-
 int	main(int argc, char **argv)
 {
 	pid_t	pid;
 	int		idx;
-	int		cnt;
+	int		div;
 
 	if (argc != 3)
 		return (0);
 	pid = ft_atoi(argv[1]);
 	idx = 0;
-	cnt = 0;
 	while (argv[2][idx])
 	{
-		send(argv[2][idx++], pid);
-		if (cnt++ % 101 == 100)
-			send(127, pid);
+		div = 128;
+		while (div)
+		{
+			if (argv[2][idx] & div)
+				kill(pid, SIGUSR1);
+			else
+				kill(pid, SIGUSR2);
+			div /= 2;
+			usleep(100);
+		}
+		idx++;
 	}
-	send(127, pid);
 	return (0);
 }
--- server.c
+++ server.c
@@ -6,57 +6,40 @@
 /*   By: hahn <hahn@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/27 16:34:54 by hahn              #+#    #+#             */
-/*   Updated: 2022/07/06 10:40:12 by hahn             ###   ########.fr       */
+/*   Updated: 2022/07/06 13:51:13 by hahn             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
-char	*make_str(char *str, char c)
-{
-	char	*ret;
-	int		idx;
-
-	idx = 0;
-	ret = (char *)malloc(sizeof(char) * ft_strlen(str) + 2);
-	while (str[idx])
-	{
-		ret[idx] = str[idx];
-		idx++;
-	}
-	free(str);
-	ret[idx++] = c;
-	ret[idx] = '\0';
-	return (ret);
-}
-
 void	handler(int signum)
 {
 	static int	count = 7;
 	static int	num = 0;
-	static char	*str;
 
 	num += signum == SIGUSR1;
-	if (!str)
+	count--;
+	if (count == -1)
 	{
-		str = (char *)malloc(sizeof(char) * 1);
-		str[0] = '\0';
-	}
-	if (--count == -1)
-	{
-		if (num != 127)
-			str = make_str(str, num);
-		else
-		{
-			write(1, str, ft_strlen(str));
-			free(str);
-			str = NULL;
-		}
+		write(1, &num, 1);
 		count = 7;
 		num = 0;
 	}
 	else
 		num *= 2;
+}
+
+int	main(void)
+{
+	char	*pid;
+
+	pid = ft_itoa(getpid());
+	ft_putendl_fd(pid, 1);
+	signal(SIGUSR1, handler);
+	signal(SIGUSR2, handler);
+	while (1)
+		pause();
+	return (0);
 }
 
 int	main(void)
